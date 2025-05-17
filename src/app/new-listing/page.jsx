7659ab34613c78fd4e130c4bfd53bb292c0afb43
pabi5057 <pabitra.panda.5057@gmail.com>
2025-05-17 17:23:57 +0530

    added category based features

--- src/app/new-listing/page.jsx
+++ src/app/new-listing/page.jsx
@@ -118,7 +118,7 @@ export default function NewListingPage() {
         callback(filtered);
     }, 300);
 
-    if (!hasMounted) return null; // Prevent hydration error
+    if (!hasMounted) return null; 
 
     return (
         <>
